7dd9a7f49f9382fc2989667d18d849b823b28819
david <077502021@mail.ru>
2018-01-03 19:16:09 +0400

    Make more readable

--- views/about.view.php
+++
@@ -1,5 +0,0 @@
-<?php require('partials/head.php')?>
-<div class="content">
-    <p>My about page</p>
-</div>
-<?php require('partials/footer.php')?>
--- views/index.view.php
+++
@@ -1,9 +0,0 @@
-<?php require('partials/head.php') ?>
-    <div class="content">
-        <ul>
-            <?php foreach ($users as $user): ?>
-                <li><?= $user->first_name ?></li>
-            <?php endforeach; ?>
-        </ul>
-    </div>
-<?php require('partials/footer.php') ?>
